8fb1509625993bf5c59e6e54c66efdff2a659997
Diego Sabillon <A01798446@tec.mx>
2024-10-22 09:31:46 -0600

    Prueba

--- PlanSnap/Views/FotosView.swift
+++ PlanSnap/Views/FotosView.swift
@@ -4,12 +4,34 @@ import SwiftUI
 
 struct FotosView: View {
     var body: some View {
-        VStack {
-            Text("Fotos")
+        NavigationView {
+            VStack(spacing: 20) {
+                Spacer()
+                
+                Text("Sube Tu Planta!")
+                    .font(.system(size: 45))
+                    .fontWeight(.bold)
+                    .foregroundColor(Color(red: 34/255, green: 34/255, blue: 34/255))
+                
+                Image("logo_plant")
+                    .resizable(resizingMode: .stretch)
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 300, height: 200)
+                    
+                NavigationLink(destination: FotosView()) {
+                    Text("Tomar Foto")
+                        .font(.headline)
+                        .frame(width: 200, height: 50)
+                        .background(Color(red: 50/255, green: 205/255, blue: 50/255))
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                }
+                Spacer()
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(Color(red: 205/255, green: 184/255, blue: 157/255))
+            .edgesIgnoringSafeArea(.all)
         }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .background(Color(red: 205/255, green: 184/255, blue: 157/255))
-        .edgesIgnoringSafeArea(.all)
     }
 }
 
