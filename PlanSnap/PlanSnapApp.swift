6c592b16ba904eb3015cdc8b8ab46c0aa509a19c
Kevin <kevings2004@hotmail.com>
2024-10-21 18:10:50 -0600

    Initial Commit

---
+++ PlanSnap/PlanSnapApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlanSnapApp.swift
+//  PlanSnap
+//
+//  Created by Kevin Garcia on 21/10/24.
+//
+
+import SwiftUI
+
+@main
+struct PlanSnapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
