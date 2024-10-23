308ce1524152b737127477f85bac98dde1635ba7
Rafael Hernandez <a00836774@tec.mx>
2024-10-23 10:23:50 -0600

    hola

---
+++ PlanSnap/Views/InformationView.swift
@@ -0,0 +1,18 @@
+//
+//  InformationView.swift
+//  PlanSnap
+//
+//  Created by Rafael Lorenzo Hernandez  on 23/10/24.
+//
+
+import SwiftUI
+
+struct InformationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InformationView()
+}
---
+++ PlanSnap/Views/LearningView.swift
@@ -0,0 +1,18 @@
+//
+//  LearningView.swift
+//  PlanSnap
+//
+//  Created by Rafael Lorenzo Hernandez  on 23/10/24.
+//
+
+import SwiftUI
+
+struct LearningView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LearningView()
+}
