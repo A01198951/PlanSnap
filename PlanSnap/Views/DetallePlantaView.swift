6bc34d25062dda0ccf1ae1655e5fb44c03e649d1
Oscar21122 <oscarzhaoxu@gmail.com>
2024-10-23 21:36:05 -0600

    Cambios en diseño de la info de la planta y en el acceso de la vista de info desde el tab bar

--- PlanSnap/Views/DetallePlantaView.swift
+++ PlanSnap/Views/DetallePlantaView.swift
@@ -7,42 +7,63 @@ import Vision
 import UIKit
 import VideoToolbox
 
-
 struct DetallePlantaView: View {
     let image: UIImage
     @Query private var plantas: [Planta]
     @Binding var historial: [PlantaH]
     @State private var plantaIdentificada: String = "Preparando análisis..."
     @State private var errorMessage: String?
-    @State private var isLoading: Bool = false
     @State private var plantaDescripcion: String = ""
     @State private var plantaUso: String = ""
-    
+    @State private var isClassified: Bool = false
+
     var body: some View {
         VStack(spacing: 20) {
             Text(plantaIdentificada)
                 .font(.system(size: 40, weight: .bold))
                 .foregroundColor(.black)
-                .padding(.top, 70)
-            
+                .padding(.top, 120)
+
             Image(uiImage: image)
                 .resizable()
                 .aspectRatio(contentMode: .fit)
-                .frame(maxWidth: 300, maxHeight: 300)
+                .frame(maxWidth: 200, maxHeight: 200)
                 .cornerRadius(10)
                 .padding()
 
-            if isLoading {
-                ProgressView()
-                    .scaleEffect(1.5)
-                    .padding()
-            }
-            
             if let errorMessage = errorMessage {
                 Text("Error: \(errorMessage)")
                     .foregroundColor(.red)
-                    .padding()
+                    .padding(.horizontal, 80)
+                    .multilineTextAlignment(.center)
             }
+
+            if !plantaDescripcion.isEmpty || !plantaUso.isEmpty {
+                VStack(alignment: .center, spacing: 10) {
+                    if !plantaDescripcion.isEmpty {
+                        Text("Descripción:")
+                            .font(.headline)
+                            .foregroundColor(.black)
+                        Text(plantaDescripcion)
+                            .font(.body)
+                            .foregroundColor(.black)
+                            .multilineTextAlignment(.center)
+                    }
+
+                    if !plantaUso.isEmpty {
+                        Text("Uso:")
+                            .font(.headline)
+                            .foregroundColor(.black)
+                        Text(plantaUso)
+                            .font(.body)
+                            .foregroundColor(.black)
+                            .multilineTextAlignment(.center)
+                    }
+                }
+                .padding()
+            }
+
+            Spacer()
             
             if errorMessage != nil {
                 Button("Reintentar") {
@@ -52,73 +73,51 @@ struct DetallePlantaView: View {
                 .background(Color.blue)
                 .foregroundColor(.white)
                 .cornerRadius(10)
+                .padding(.bottom, 140)
             }
-            
-            // Mostrar descripción y uso
-            if !plantaDescripcion.isEmpty {
-                VStack(alignment: .leading, spacing: 10) {
-                    Text("Descripción:")
-                        .font(.headline)
-                        .foregroundColor(.black)
-                    Text(plantaDescripcion)
-                        .font(.body)
-                        .foregroundColor(.black)
-                    
-                    Text("Uso:")
-                        .font(.headline)
-                        .foregroundColor(.black)
-                    Text(plantaUso)
-                        .font(.body)
-                        .foregroundColor(.black)
-                }
-                .padding()
-            }
-            
-            Spacer()
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(Color(red: 205/255, green: 184/255, blue: 157/255))
         .edgesIgnoringSafeArea(.all)
         .onAppear {
-            clasificarPlanta()
+            if !isClassified {
+                clasificarPlanta()
+            }
         }
     }
-    
+
     func prepararImagen(_ imagen: UIImage) -> CGImage? {
         let targetSize = CGSize(width: 299, height: 299)
-        
+
         UIGraphicsBeginImageContextWithOptions(targetSize, true, 1.0)
         imagen.draw(in: CGRect(origin: .zero, size: targetSize))
         let resizedImage = UIGraphicsGetImageFromCurrentImageContext()
         UIGraphicsEndImageContext()
-        
+
         return resizedImage?.cgImage
     }
-    
+
     func clasificarPlanta() {
-        isLoading = true
         errorMessage = nil
         plantaIdentificada = "Analizando..."
         plantaDescripcion = ""
         plantaUso = ""
-        
+
         guard let cgImage = prepararImagen(image) else {
             errorMessage = "Error al procesar la imagen"
-            isLoading = false
             return
         }
-        
+
         guard let modelURL = Bundle.main.url(forResource: "PlantClassificationR", withExtension: "mlmodelc") else {
             print("No se encontró el archivo del modelo")
             errorMessage = "No se encontró el modelo de clasificación"
-            isLoading = false
             return
         }
-        
+
         do {
             let config = MLModelConfiguration()
             let model = try MLModel(contentsOf: modelURL, configuration: config)
-            
+
             guard let input = try? MLFeatureValue(
                 cgImage: cgImage,
                 pixelsWide: 299,
@@ -127,27 +126,26 @@ struct DetallePlantaView: View {
                 options: nil
             ) else {
                 errorMessage = "Error al preparar la imagen para el modelo"
-                isLoading = false
                 return
             }
-            
+
             let inputName = model.modelDescription.inputDescriptionsByName.first?.key ?? "image"
             let inputFeatures = try MLDictionaryFeatureProvider(dictionary: [inputName: input])
-            
+
             let prediction = try model.prediction(from: inputFeatures)
-            
-            // Agrega una constante para el umbral de confianza
-            let umbralConfianza: Double = 0.3 // 40% 
+
+            let umbralConfianza: Double = 0.5 // 40%
 
             // En el método clasificarPlanta()
             if let outputFeatures = prediction.featureValue(for: "targetProbability")?.dictionaryValue as? [String: Double] {
                 // Filtrar las predicciones que superen el umbral de confianza
                 let filteredPredictions = outputFeatures.filter { $0.value >= umbralConfianza }
-                
+
                 // Si hay predicciones válidas, tomar la mejor
                 if let bestPrediction = filteredPredictions.max(by: { $0.value < $1.value }) {
                     DispatchQueue.main.async {
                         plantaIdentificada = bestPrediction.key
+                        isClassified = true
 
                         // Buscar la planta identificada en la lista de plantas
                         if let plantaEncontrada = plantas.first(where: { $0.nombre.lowercased() == plantaIdentificada.lowercased() }) {
@@ -168,10 +166,9 @@ struct DetallePlantaView: View {
             } else {
                 throw NSError(domain: "Predicción", code: -1, userInfo: [NSLocalizedDescriptionKey: "No se pudieron obtener las probabilidades"])
             }
-            
+
         } catch {
             DispatchQueue.main.async {
-                isLoading = false
                 print("Error al procesar: \(error)")
                 errorMessage = "Error al procesar la imagen: \(error.localizedDescription)"
             }
@@ -182,3 +179,4 @@ struct DetallePlantaView: View {
 #Preview {
     DetallePlantaView(image: UIImage(systemName: "leaf")!, historial: .constant([]))
 }
+
